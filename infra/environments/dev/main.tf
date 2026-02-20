452f20f08bba2bb36f3c9291bc0e70c34a715238
KeeganDavis <keegan.davis96@gmail.com>
2026-02-20 10:43:53 -0600

    feat: VPC TF module added to dev and services.tf created to init APIs

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -1,1 +1,17 @@
 data "google_client_config" "default" {}
+
+# Call the VPC module
+module "vpc" {
+  source = "../../modules/vpc"
+
+  project_id   = var.project_id
+  region       = var.region
+  network_name = "gcp-gitops-dev-vpc"
+  subnet_name  = "gcp-gitops-dev-subnet"
+  subnet_cidr  = "10.0.0.0/16"
+
+  # Wait for the APIs to be enabled before creating the VPC
+  depends_on = [
+    google_project_service.enabled_apis
+  ]
+}
---
+++ infra/environments/dev/services.tf
@@ -0,0 +1,24 @@
+# A list of all the GCP APIs we will need for this capstone
+variable "gcp_service_list" {
+  description = "The list of apis necessary for the project"
+  type        = list(string)
+  default = [
+    "compute.googleapis.com",              # Required for VPC and Compute
+    "container.googleapis.com",            # Required for GKE
+    "artifactregistry.googleapis.com",     # Required for Docker Images
+    "iamcredentials.googleapis.com",       # Required for Workload Identity Federation
+    "sts.googleapis.com"                   # Required for Security Token Service (WIF)
+  ]
+}
+
+# Enable all APIs listed above
+resource "google_project_service" "enabled_apis" {
+  for_each = toset(var.gcp_service_list)
+
+  project = var.project_id
+  service = each.key
+
+  # Prevents Terraform from destroying the API and dependent resources on destroy
+  disable_on_destroy         = false
+  disable_dependent_services = false
+}
