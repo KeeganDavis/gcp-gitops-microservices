bdd9c5a14b425bf8eeae324cc12dbdad0f4af4f2
KeeganDavis <keegan.davis96@gmail.com>
2026-02-20 11:48:59 -0600

    feat: gke module instantiated in dev

--- infra/environments/dev/variables.tf
+++ infra/environments/dev/variables.tf
@@ -8,3 +8,9 @@ variable "region" {
   type        = string
   default     = "us-central1"
 }
+
+variable "zone" {
+  description = "The default GCP zone for resources"
+  type        = string
+  default     = "us-central1-a"
+}
