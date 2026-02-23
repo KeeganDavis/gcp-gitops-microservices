29eb7507da3a80940a18714e8b05a4d7e4dfb316
Keegan <keegan.davis96@gmail.com>
2026-02-23 13:22:09 -0600

    chore/test-tfsec (#10)
    * chore: trigger tfsec pipeline
    
    * fix: updated pathing in terraform-pr.yaml infrastructure -> infra

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -10,6 +10,9 @@ module "vpc" {
   subnet_name  = "capstone-dev-subnet"
   subnet_cidr  = "10.0.0.0/16"
 
+  # Added to trigger tfsec pipeline
+  # environment = "dev"
+
   # Wait for the APIs to be enabled before creating the VPC
   depends_on = [
     google_project_service.enabled_apis
@@ -26,6 +29,9 @@ module "gke" {
   # Pass the network identifiers from the VPC module outputs
   network_name = module.vpc.network_name
   subnet_name  = module.vpc.subnet_name
+
+  # Added to trigger tfsec pipeline
+  # environment = "dev" 
 
   # Wait for the VPC to be fully built before trying to put a cluster inside it
   depends_on = [
