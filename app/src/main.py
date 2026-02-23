d5f609f26a5d3ff0b1de2d3f7aba61689c71cdba
KeeganDavis <keegan.davis96@gmail.com>
2026-02-23 15:42:43 -0600

    feat: added local opentelemetry trace forwarding to Jaeger

--- app/src/main.py
+++ app/src/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI, Response, status
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 import time
 
 # Initialize tracing pipeline
 provider = TracerProvider()
-processor = BatchSpanProcessor(ConsoleSpanExporter())
+otlp_exporter = OTLPSpanExporter() 
+processor = BatchSpanProcessor(otlp_exporter)
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 
